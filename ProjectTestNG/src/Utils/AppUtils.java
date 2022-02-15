ffc8684fd610a64e1d5497d178c1a73fc6a4e99e
kavitahiremath <kavitas.khedalkar@gmail.com>
2022-02-15 16:19:05 +0530

    Commit stmt

---
+++ ProjectTestNG/src/Utils/AppUtils.java
@@ -0,0 +1,24 @@
+package Utils;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class AppUtils {
+  @Test
+  public void f() {
+	  System.out.println("This is test"); 
+	  System.out.println("This is test 1"); 
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("This is before test"); 
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("This is After test"); 
+  }
+
+}
